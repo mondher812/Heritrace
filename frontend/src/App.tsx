192501bdd9ad195af7d88c6570c1a85f77d8eac5
izemrane <Mondher.izemrane@mail.utoronto.ca>
2025-02-15 16:19:37 -0500

    new-page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './pages/landing';
 import RotatingFlagsLoader from './pages/loading_page';
 import NewLanding from './pages/new_landing'
+import NewPage from './pages/new_page';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Route path="/" element={<NewLanding />} />
         <Route path="/name" element={<Landing />} />
         <Route path="/loading" element={<RotatingFlagsLoader />} />
+        <Route path="/new-page" element={<NewPage />} />
       </Routes>
     </Router>
   );
