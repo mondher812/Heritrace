ace0972a218e71a049f5230e6984a24b33aaea7b
Arsen Cameron <arsencameron@Arsens-MacBook-Pro.local>
2025-02-15 00:55:12 -0500

    linking everything

--- frontend/src/components/submit-button.tsx
+++ frontend/src/components/submit-button.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './submit-button.css';
 
 const SubmitButton: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/loading');
+  };
+
   return (
-    <button className="submit-button" type="submit">
+    <button className="submit-button" type="button" onClick={handleClick}>
       Submit
     </button>
   );
